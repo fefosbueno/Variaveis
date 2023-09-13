a7ec54a9acc9dc88dce110770fdae1a45a66acf8
felipe bueno <felipe_carvalho_bueno@hotmail.com>
2023-09-13 20:13:58 -0300

    exercicio

---
+++ variaveis-aritimeticos.js
@@ -0,0 +1,15 @@
+let x = 10, y = 20;
+
+let resultado = x + y;
+
+console.log(resultado);
+
+resultado = x - y;
+console.log(resultado);
+
+resultado = x * y;
+console.log(resultado);
+
+resultado = x / y;
+console.log(resultado);
+
